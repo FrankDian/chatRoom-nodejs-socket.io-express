13a813b46ab799a6adf2bb160b3495f8425aaf66
FrankDian <1355253924@qq.com>
2016-08-29 12:48:29 +0800

    小修改
    小修改

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
  */
 var express = require('express');
 var path = require('path');
+
+//A router object is an isolated instance of middleware and routes. You can think of it as a “mini-application,”
 var router = express.Router();
 var app = express();//调用框架
 var http = require('http').Server(app);//实例一个http服务
@@ -14,13 +16,13 @@ var roomInfo = {};
 
 //指定静态文本存储的地方
 app.use(express.static( path.join(__dirname, 'public' ) ) );
-app.set("views", path.join(__dirname , 'views'));
-app.set("view engine","hbs");
+app.set("views", path.join(__dirname , 'views'));//A directory or an array of directories for the application's views.
+app.set("view engine","hbs");//The default engine extension to use when omitted.
 
  // 获取请求建立socket连接的url
   // 如: http://localhost:3000/room/room_1, roomID为room_1
 io.on("connection" ,function(socket){//用户连接事件
-	var url = socket.request.headers.referer;///////////////////////////////////////
+	var url = socket.request.headers.referer;//socket.request.headers.cookie
 	var splited = url.split("/");
 	var roomID = splited[splited.length -1];//房间ID
 	var user = "";//用户名
@@ -36,7 +38,7 @@ io.on("connection" ,function(socket){//用户连接事件
 		}
 		roomInfo[roomID].push(user);
 		//通知房内人员
-		io.to(roomID).emit('systemInfo', user + "加入了房间", roomInfo[roomID]);
+		io.to(roomID).emit('systemMsg', user + "加入了房间", roomInfo[roomID]);
 		console.log(user + "加入了" + roomID);
 	});
 	
@@ -53,31 +55,36 @@ io.on("connection" ,function(socket){//用户连接事件
 		}
 		socket.leave(roomID);
 		//通知房内人员
-		io.to(roomID).emit('systemInfo', user + "退出了房间", roomInfo[roomID]);
+		io.to(roomID).emit('systemMsg', user + "退出了房间", roomInfo[roomID]);
 		console.log(user + "退出了" + roomID);
 	});
 	
 	//3.接收消息并发送到响应的房间
 	socket.on('message',function(msg){
 		//如果用户不在此房间内则不发送消息
-		if( roomInfo[roomID].indexOf(user) == -1){
-			io.to(roomID).emit( 'msg', user , msg );
+		if( roomInfo[roomID].indexOf(user) === -1){
+			return false;
 		}
+		console.log(user + ":" + msg);
+		io.to(roomID).emit( 'msg', user , msg );
 	});
 });
 
 //房间页面
-router.get( '/room/:roomID' , function(req,res){
+//Renders a view and sends the rendered HTML string to the client. Optional parameters:
+router.get( '/room/:roomID' , function(req,res){//同 app.get
 	var roomID = req.params.roomID;
 	
 	//渲染页面数据 views/room.hbs
-	res.render('room',{
+	res.render('room',{//pass a local variable to the view
 		roomID : roomID,
 		users : roomInfo[roomID]
 	});
 });
 
-app.use('/',router);/////////////////////////////////////////////////////
+// only requests to /* will be sent to our "router"
+//所有的网址请求都会通过router处理
+app.use('/',router);
 
 http.listen(8888,function(){
 	console.log('listening on port 8888');
